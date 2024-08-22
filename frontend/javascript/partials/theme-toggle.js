04d1e81f641965ce02aef76375758a69c75869ac
Tina Bell Vance <tina@bellvance.com>
2024-08-21 22:22:08 -0400

    Add favicon

--- frontend/javascript/partials/theme-toggle.js
+++ frontend/javascript/partials/theme-toggle.js
@@ -1,38 +1,27 @@
-const themeToggle = document.querySelector(
-  '[data-target="theme-toggle-button"]',
-);
+const themeToggle = document.querySelector('[data-target="theme-toggle-button"]');
 const favicon = document.querySelector("link[rel='icon']");
 
 const currentTheme = localStorage.getItem("theme") ?? "darkMode";
-const pageTheme = document.getElementsByTagName("html")[0];
+const pageTheme = document.documentElement;
 const preferredTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 let isDarkMode = currentTheme === "darkMode";
 
 favicon.href = preferredTheme ? "/images/favicon-white.ico" : "/images/favicon-black.ico";
-if (currentTheme === "darkMode") {
-  pageTheme.classList.replace("theme-light", "theme--dark");
-  themeToggle.classList.add("to-dark");
-} else {
-  pageTheme.classList.replace("theme--dark", "theme--light");
-  themeToggle.classList.add("to-light");
-}
 
-function themeMode() {
+pageTheme.classList.toggle("theme--dark", isDarkMode);
+pageTheme.classList.toggle("theme--light", !isDarkMode);
+themeToggle.classList.toggle("to-dark", isDarkMode);
+themeToggle.classList.toggle("to-light", !isDarkMode);
+
+function toggleTheme() {
   isDarkMode = !isDarkMode;
-  const themeClass = isDarkMode ? "theme--dark" : "theme--light";
-  const themeToggleClass = isDarkMode ? "to-dark" : "to-light";
-  const theme = isDarkMode ? "darkMode" : "lightMode";
-  pageTheme.classList.replace(
-    isDarkMode ? "theme--light" : "theme--dark",
-    themeClass
-  );
-  themeToggle.classList.replace(
-    isDarkMode ? "to-light" : "to-dark",
-    themeToggleClass
-  );
-
-  localStorage.setItem("theme", theme);
+  pageTheme.classList.toggle("theme--dark");
+  pageTheme.classList.toggle("theme--light");
+  themeToggle.classList.toggle("to-dark");
+  themeToggle.classList.toggle("to-light");
+  localStorage.setItem("theme", isDarkMode ? "darkMode" : "lightMode");
 }
 
-themeToggle.addEventListener("click", themeMode);
+themeToggle.addEventListener("click", toggleTheme);
+
